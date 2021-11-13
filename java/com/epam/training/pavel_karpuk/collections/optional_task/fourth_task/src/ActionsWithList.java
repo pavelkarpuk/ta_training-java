0c7a0f2e372cd95a6a93a628df633681cf861fbe
Pavel Karpuk <pavel.karpuk.96@mail.ru>
2021-11-13 21:40:32 +0300

    add Fourth optional task from Java.Collections

---
+++ java/com/epam/training/pavel_karpuk/collections/optional_task/fourth_task/src/ActionsWithList.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+
+public class ActionsWithList {
+
+    public List<String> readsLinesFromFileToList() throws IOException {
+        return Files.readAllLines(Paths.get("C:\\java\\com\\epam\\training\\pavel_karpuk\\collections\\optional_task\\fourth_task\\Poem.txt"));
+    }
+
+    public void sortByLinesLength (List<String> lines) {
+        lines.sort(Comparator.comparingInt(String::length));
+    }
+}
